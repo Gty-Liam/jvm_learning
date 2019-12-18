4abe06fa582bc85dce72b003acc9afe9ae733f8b
Liam <244322356@qq.com>
2019-12-18 19:36:42 +0800

    JVM Shangguigu

--- src/pers/liam/jvm_learning/T04.java
+++ src/pers/liam/jvm_learning/T04.java
@@ -1,4 +1,13 @@
 package pers.liam.jvm_learning;
 
 public class T04 {
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().availableProcessors()); // 查询核数
+
+        long maxMemory = Runtime.getRuntime().maxMemory();
+        long totalMemory = Runtime.getRuntime().totalMemory();
+
+        System.out.println("-Xmx: maxMemory: " + (maxMemory / 1024 / 1024) + " MB");
+        System.out.println("-Xms: totalMemory: " + (totalMemory / 1024 / 1024) + " MB");
+    }
 }
