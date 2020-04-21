78b26b838d8dba580fcedf31c1b757ed1edef178
Liam <244322356@qq.com>
2020-04-21 12:30:13 +0800

    JVM part2

--- src/pers/liam/gc/Test.java
+++ src/pers/liam/gc/Test.java
@@ -6,8 +6,9 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("....");
         String str = new String("");
+        Random r = new Random();
         while(true){
-            str += new Random().nextInt(101806400) + "123dfss";
+            str = str + str + r.nextInt(101806400) + "123dfss";
         }
 //        try{
 //            Thread.sleep(Integer.MAX_VALUE);
